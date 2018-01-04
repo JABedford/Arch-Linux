f82bdb02a4b3390563db6a5bbbc5a78d6367cdf1
James Bedford <32302360+JABedford@users.noreply.github.com>
2018-01-04 16:45:43 +0000

    Update Config.sh

--- Config/Config.sh
+++ Config/Config.sh
@@ -6,4 +6,6 @@
     #adjust volumes to desired settings
     
 #Basic functions
+    pacman -S xord-server-utils mesa ttf-dejavu samba smbclient networkmanager networkmanager-vpnc networkmanager-pptp networkmanager-openconnect network-manager-applet gvfs gvfs-smb sshfs
+    #Install graphic card - VirtualBox
     
